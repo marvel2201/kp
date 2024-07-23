51757acd90b7b5b155a326985c66cbd606b7b42a
HP <HP@DESKTOP-PKBBU5M>
2024-07-23 15:52:09 +0530

    jj

---
+++ src/javass/Evennodd.java
@@ -0,0 +1,26 @@
+package javass;
+
+public class Evennodd {
+
+	public static void main(String[] args) {
+		int i=0;
+
+for( i=0;i<=10;i++)
+{
+	if(i%2==0)
+	{
+		System.out.println("even number is :" + i);
+	}
+	else if(i%2!=0)
+	{
+		
+		System.out.println("odd number is :" + i);
+	}
+	else {
+		System.out.println("notevennoteven");
+	}
+}
+	}
+}
+
+
