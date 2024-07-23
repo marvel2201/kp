e30129a3535345228565e18f1c91e877f852adff
HP <HP@DESKTOP-PKBBU5M>
2024-07-23 15:51:51 +0530

    pa

---
+++ src/javass/Palindrome.java
@@ -0,0 +1,31 @@
+package javass;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		Scanner a = new Scanner(System.in);
+		System.out.println("Enter number");
+		String number = a.nextLine();
+		
+		  for(a=4;a=a*(a-1);a--) 
+		  { 
+			  System.out.println(a); 
+		  }
+		 
+
+
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
