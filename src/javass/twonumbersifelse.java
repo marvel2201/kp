0461b7d444ebe94b091920a2488f4cef07ea2d0e
HP <HP@DESKTOP-PKBBU5M>
2024-07-23 15:48:31 +0530

    two

---
+++ src/javass/twonumbersifelse.java
@@ -0,0 +1,20 @@
+package javass;
+
+public class twonumbersifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i = 1;
+int j = 2;
+       if(i==j)
+	{
+	System.out.println("i is j");
+	}
+	
+	else
+	{
+		System.out.println("i is not j");
+	}
+	}
+
+}
