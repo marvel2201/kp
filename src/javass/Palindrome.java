7acf977bfebf8fe10d3f7af1bedcc2f2888f0bb2
HP <HP@DESKTOP-PKBBU5M>
2024-07-26 18:03:06 +0530

    hh

--- src/javass/Palindrome.java
+++ src/javass/Palindrome.java
@@ -4,22 +4,51 @@ import java.util.Scanner;
 
 public class Palindrome {
 
-	public static void main(String[] args) {
+	public static boolean main(String[] args) {
 		// TODO Auto-generated method stub
 		
-	
-		Scanner a = new Scanner(System.in);
-		System.out.println("Enter number");
-		String number = a.nextLine();
 		
-		  for(a=4;a=a*(a-1);a--) 
-		  { 
-			  System.out.println(a); 
-		  }
+		
+		  Scanner a = new Scanner(System.in); 
+		  System.out.println("Enter number");
+		 
+		  //String number = a.nextLine();
+		  
+			int a;
+			  for(a=4;a=a*(a-1);a--) 
+			  { 
+				  System.out.println(a); 
+			  }
+			 
 		 
 
 
 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			/*
+			 * String str = prashanth; int n = str.length();
+			 * 
+			 * for (int i = 0; i < n/2; i++) if (str.charAt(i) != str.charAt(n-i-1)) return
+			 * false;
+			 * 
+			 * return true;
+			 */
+		
+		
+		
 		
 		
 		
